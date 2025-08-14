cf093c2bc7a2810d7e9c66ded5dc6f10a0e38827
james <ivanlavan773@gmail.com>
2025-08-14 09:16:08 +0300

    added the name

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,24 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="fido", breed="Human"):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed 
+    @breed.setter
